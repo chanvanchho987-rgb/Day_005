55f8fe7e4ce0fbe53413109dddbb8ee331600ef1
sva <chanvanchho987@gmail.com>
2026-05-07 17:00:43 +0700

    u

---
+++ Controller.java
@@ -0,0 +1,20 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private Button button_calculate;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    private TextField textfile_num1;
+
+    @FXML
+    private TextField textfile_num2;
+
+}
