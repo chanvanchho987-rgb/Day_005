f2df7bc956f5b475c481f95617e3c6b09816c47d
Tek Lyhai <lyhai120212@gmail.com>
2026-05-21 16:14:44 +0700

    jn
    jn

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
     public static void main(String[] args) {
+        launch();
         
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 }
