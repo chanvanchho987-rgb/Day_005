daa639de6f4a1436ac6f4d5ae57de3df1e766a1a
Jasonop89 <sussybakagonnamakemeactup123@gmail.com>
2026-05-21 17:01:36 +0700

    Revert "Merge branch 'main' of https://github.com/chanvanchho987-rgb/Day_005"
    This reverts commit e81fbeb1bdda54c791f2b50933ea200b3d1bfb2b, reversing
    changes made to e4f4ab03e12e585ec93a86ae02b98b549273d3d9.

--- Main.java
+++ Main.java
@@ -18,11 +18,5 @@ public class Main extends Application{
         FXMLLoader loader = new FXMLLoader();
         
         loader.setLocation(getClass().getResource("View.fxml"));
-
-        Scene scene = new Scene(loader.load());
-
-        stage.setScene(scene);
-
-        stage.show();
     }
 }
