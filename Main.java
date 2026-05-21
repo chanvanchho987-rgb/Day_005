da7a51d5121c8b02bbd637ca773e2cd19533d599
sva <chanvanchho987@gmail.com>
2026-05-21 16:30:50 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,5 +18,10 @@ public class Main extends Application{
         FXMLLoader loader = new FXMLLoader();
         
         loader.setLocation(getClass().getResource("View.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
