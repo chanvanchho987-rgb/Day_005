bef4c948dd227d92c735c3d275ae454573626ddf
sva <chanvanchho987@gmail.com>
2026-05-25 23:22:10 +0700

    update

--- Controller.java
+++ Controller.java
@@ -4,19 +4,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class Controller {
+public class controller {
 
     @FXML
-    private Button button_calculate;
+    private Button btn_calculate;
 
     @FXML
     private Label label_result;
 
     @FXML
-    private TextField textfile_num1;
+    private TextField textfeild_num1;
 
     @FXML
-    private TextField textfile_num2;
+    private TextField textfield_num2;
 
     @FXML
     void on_calculate(ActionEvent event) {
--- Main.java
+++ Main.java
@@ -1,22 +1,20 @@
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
     public static void main(String[] args) {
         launch();
-        
     }
 
     @Override
-    public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
-        
+    public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        
-        loader.setLocation(getClass().getResource("View.fxml"));
+        loader.setLocation(getClass().getResource("view.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
