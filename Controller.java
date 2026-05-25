55c3a274844f1ea5366787487751ca284ab2b547
sva <chanvanchho987@gmail.com>
2026-05-25 23:41:07 +0700

    update

--- Controller.java
+++ Controller.java
@@ -2,7 +2,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class controller {
 
@@ -10,13 +9,13 @@ public class controller {
     private Button btn_calculate;
 
     @FXML
+    private Label label_num1;
+
+    @FXML
+    private Label label_num2;
+
+    @FXML
     private Label label_result;
-
-    @FXML
-    private TextField textfeild_num1;
-
-    @FXML
-    private TextField textfield_num2;
 
     @FXML
     void on_calculate(ActionEvent event) {
