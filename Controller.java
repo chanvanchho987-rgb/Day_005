bee60e203e5365bee126f8b3d4bf908467a8256a
sva <chanvanchho987@gmail.com>
2026-05-26 21:02:56 +0700

    u

--- Controller.java
+++ Controller.java
@@ -3,7 +3,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class controller {
+public class Controller {
 
     @FXML
     private Button btn_calculate;
