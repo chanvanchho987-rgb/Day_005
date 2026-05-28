0e49e95ec564043043f98c8e274b3e60cb58c13c
sva <chanvanchho987@gmail.com>
2026-05-28 15:15:30 +0700

    udate

--- Controller.java
+++ Controller.java
@@ -4,7 +4,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 
-public class Controller {
+public class controller {
 
     @FXML
     private Button btn_calculate;
--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("view.fxml"));
+        loader.setLocation(getClass().getResource("View.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
