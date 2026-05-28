c2480a77b258a12a79734835470179552345fc8a
sva <chanvanchho987@gmail.com>
2026-05-28 19:51:46 +0700

    u

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("main/View.fxml"));
+        loader.setLocation(getClass().getResource("View.fxml"));
         Scene scene = new Scene(loader.load());
 
         stage.setScene(scene);
